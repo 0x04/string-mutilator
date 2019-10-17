44fc566278bcea09656a192e75b128ffe00398be
0x04 <ok@0x04.de>
2019-10-18 01:08:32 +0200

    fix: default export

--- src/index.js
+++ src/index.js
@@ -5,17 +5,43 @@
 
 import * as charCase from './char-case';
 import * as compressor from './compressor';
+import flipBits from './flip-bits';
+import jumble from './jumble';
+import reverse from './reverse';
+import reverseBits from './reverse-bits';
+import rockdotize from './rockdotize';
+import rot13 from './rot13';
+import scramble from './scramble';
+import shift from './shift';
+import shiftBits from './shift-bits';
+import toMANS from './to-mans';
 
-export { charCase };
-export { compressor };
+export default {
+  charCase,
+  compressor,
+  flipBits,
+  jumble,
+  reverse,
+  reverseBits,
+  rockdotize,
+  rot13,
+  scramble,
+  shift,
+  shiftBits,
+  toMANS
+};
 
-export { default as flipBits } from './flip-bits';
-export { default as jumble } from './jumble';
-export { default as reverse } from './reverse';
-export { default as reverseBits } from './reverse-bits';
-export { default as rockdotize } from './rockdotize';
-export { default as rot13 } from './rot13';
-export { default as scramble } from './scramble';
-export { default as shift } from './shift';
-export { default as shiftBits } from './shift-bits';
-export { default as toMANS } from './to-mans';
+export {
+  charCase,
+  compressor,
+  flipBits,
+  jumble,
+  reverse,
+  reverseBits,
+  rockdotize,
+  rot13,
+  scramble,
+  shift,
+  shiftBits,
+  toMANS
+};
